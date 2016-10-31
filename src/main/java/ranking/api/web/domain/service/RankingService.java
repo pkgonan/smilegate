0c5c93f96a3dd1e863c1158360c3e959e256ba61
pkgonan <pkgonan1@naver.com>
2016-10-31 19:26:58 +0900

    완료

--- src/main/java/ranking/api/web/domain/service/RankingService.java
+++ src/main/java/ranking/api/web/domain/service/RankingService.java
@@ -11,20 +11,20 @@ public class RankingService {
     final static int PRIORITY_QUEUE_BIG_SIZE = 1000000;
     final static int PRIORITY_QUEUE_SMALL_SIZE = 10;
     final static PriorityBlockingQueue<RankingData> rankingQueue = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_BIG_SIZE, new DataComparator());
-    final static Map<String, Integer> allRankingData = new HashMap<String, Integer>();
-    final static Map<String, LinkedList<String>> myFriend = new HashMap<String, LinkedList<String>>();
+    final static Map<String, Integer> rankingData = new HashMap<String, Integer>();
+    final static Map<String, LinkedList<String>> friend = new HashMap<String, LinkedList<String>>();
 
     public void insert(String id, int score) {
-        allRankingData.put(id, score);
-        if(!myFriend.containsKey(id)) {
-            myFriend.put(id, new LinkedList<String>());
+        rankingData.put(id, score);
+        if(!friend.containsKey(id)) {
+            friend.put(id, new LinkedList<String>());
             rankingQueue.add(new RankingData(id, score));
         }
         else {
-            final Iterator<RankingData> iterator = rankingQueue.iterator();
-            while(iterator.hasNext()) {
-                if(iterator.next().getId().equals(id)) {
-                    iterator.remove();
+            final Iterator<RankingData> rankingQueueIterator = rankingQueue.iterator();
+            while(rankingQueueIterator.hasNext()) {
+                if(rankingQueueIterator.next().getId().equals(id)) {
+                    rankingQueueIterator.remove();
                     rankingQueue.add(new RankingData(id, score));
                     break;
                 }
@@ -33,14 +33,14 @@ public class RankingService {
     }
 
     public void register(String myId, String friendId) {
-        final LinkedList<String> friendList = myFriend.get(myId);
+        final LinkedList<String> friendList = friend.get(myId);
         if (!friendList.contains(friendId))
             friendList.add(friendId);
-        myFriend.put(myId, friendList);
+        friend.put(myId, friendList);
     }
 
     public void delete(String myId, String friendId) {
-        final LinkedList<String> friendList = myFriend.get(myId);
+        final LinkedList<String> friendList = friend.get(myId);
         friendList.remove(friendId);
     }
 
@@ -51,57 +51,56 @@ public class RankingService {
     }
 
     public List<String> getMyRanking(String myId) {
-        final PriorityBlockingQueue<RankingData> q = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_SMALL_SIZE, new DataComparator());
-        final LinkedList<String> linkedList = new LinkedList<String>();
+        final PriorityBlockingQueue<RankingData> myRankingQueue = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_SMALL_SIZE, new DataComparator());
+        final LinkedList<String> myRankingList = new LinkedList<String>();
         int rank = 0;
-        q.addAll(rankingQueue);
+        myRankingQueue.addAll(rankingQueue);
 
-        while(!q.isEmpty()) {
-            final RankingData rankingData = q.poll();
+        while(!myRankingQueue.isEmpty()) {
+            final RankingData rankingData = myRankingQueue.poll();
             final String id = rankingData.getId();
             int score = rankingData.getScore();
             ++rank;
 
             if(id.equals(myId)) {
                 final String myRanking = rank + ":" + id + ":" + score;
-                linkedList.add(myRanking);
+                myRankingList.add(myRanking);
                 break;
             }
         }
-
-        return linkedList;
+        return myRankingList;
     }
 
     public List<String> getFriendRankingList(String myId) {
-        final LinkedList<String> myFriendList = myFriend.get(myId);
-        final PriorityBlockingQueue<RankingData> pq = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_SMALL_SIZE, new DataComparator());
+        final LinkedList<String> myFriendList = friend.get(myId);
+        final PriorityBlockingQueue<RankingData> myFriendRankingQueue = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_SMALL_SIZE, new DataComparator());
         final Iterator<String> myFriendListIterator = myFriendList.iterator();
 
         while (myFriendListIterator.hasNext()) {
-            final String friendid = myFriendListIterator.next();
-            pq.add(new RankingData(friendid, allRankingData.get(friendid)));
+            final String friendId = myFriendListIterator.next();
+            myFriendRankingQueue.add(new RankingData(friendId, rankingData.get(friendId)));
         }
-        pq.add(new RankingData(myId, allRankingData.get(myId)));
+        myFriendRankingQueue.add(new RankingData(myId, rankingData.get(myId)));
 
-        final List<String> friendRankingList = priorityQueueToList(pq);
+        final List<String> friendRankingList = priorityQueueToList(myFriendRankingQueue);
 
         return friendRankingList;
     }
 
     public List<String> getFriendList(String myId) {
-        final LinkedList<String> myFriendList = myFriend.get(myId);
+        final LinkedList<String> myFriendList = friend.get(myId);
 
         return myFriendList;
     }
 
-    private  List<String> priorityQueueToList(PriorityBlockingQueue pq) {
-        final PriorityBlockingQueue<RankingData> q = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_BIG_SIZE, new DataComparator());
+    private  List<String> priorityQueueToList(PriorityBlockingQueue myPriorityBlockingQueue) {
+        final PriorityBlockingQueue<RankingData> priorityBlockingQueue = new PriorityBlockingQueue<RankingData>(PRIORITY_QUEUE_BIG_SIZE, new DataComparator());
         final List<String> list = new LinkedList<String>();
         int rank = 0;
-        q.addAll(pq);
+        priorityBlockingQueue.addAll(myPriorityBlockingQueue);
 
-        while(!q.isEmpty()) {
-            final RankingData rankData = q.poll();
+        while(!priorityBlockingQueue.isEmpty()) {
+            final RankingData rankData = priorityBlockingQueue.poll();
             final String id = rankData.getId();
             final int score = rankData.getScore();
             final String result = ++rank + ":" + id + ":" + score;
